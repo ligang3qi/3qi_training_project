e56599448bb92d4aa4fbe6ee8a54648797beec05
Ligang <ligang@3qilabs.com>
2017-04-11 10:38:08 -0700

    Added new caps for env.rb

--- mweb/features/support/env.rb
+++ mweb/features/support/env.rb
@@ -13,6 +13,9 @@ end
 # Load the desired configuration from appium.txt, create a driver then
 # Add the methods to the world
 caps = Appium.load_appium_txt file: File.expand_path('./', __FILE__), verbose: true
+# appium_txt = File.absolute_path(__FILE__).gsub('env.rb', 'appium.txt')
+# caps = Appium.load_settings file: appium_txt, verbose: true
+
 Appium::Driver.new(caps)
 Appium.promote_appium_methods AppiumWorld
 
